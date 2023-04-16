855dad31640ed7e21a934a3f74799f14ff6cc464
alibaligoosh <126803446+alibaligoosh@users.noreply.github.com>
2023-04-16 19:58:25 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
-VMESS_WSPATH='/vmess'
+UUID='401983d7-0890-40f0-8aba-a47a8909ba86'
+VMESS_WSPATH='https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/sub/splitted/vmess.txt'
 VLESS_WSPATH='/vless'
-TROJAN_WSPATH='/trojan'
-SS_WSPATH='/shadowsocks'
+TROJAN_WSPATH='https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/sub/splitted/trojan.txt'
+SS_WSPATH='https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/sub/splitted/ss.txt'
 NEZHA_SERVER=''
 NEZHA_PORT=''
 NEZHA_KEY=''
